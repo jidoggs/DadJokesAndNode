6cab0effae1ea8b61b699c0aa8dc06245314e8b2
jidoggs <jidoggs@gmail.com>
2021-12-13 14:19:56 +0100

    playing with dad jokes and node

---
+++ main.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const process = require("process");
+const request = require("request");
+const prompt = require("prompt");
+
+prompt.start();
+prompt.get([], (err, result) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    const options = {
+      url: `https://icanhazdadjoke.com/search?term=${result.term}`,
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    request(options, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        let resObj = JSON.parse(body);
+        if (resObj.total_jokes < 1) {
+          console.log(`No jokes are found with the word ${result.term}`);
+        } else {
+          let randomTitleJoke =
+            resObj.results[Math.floor(Math.random() * resObj.total_jokes)];
+          console.log(randomTitleJoke.joke);
+          fs.open("jokes.txt", "a", (err, fd) => {
+            if (err) {
+              console.log("There was a problem with jokes.txt");
+            } else {
+              fs.write(
+                fd,
+                `${randomTitleJoke.joke} \r\n`,
+                null,
+                "utf8",
+                (err, byteWritten) => {
+                  if (err) {
+                    console.log("there was an error writing to file");
+                  }
+                  //   else {
+                  //     console.log(`${byteWritten} characaters written`);
+                  //   }
+                }
+              );
+            }
+          });
+        }
+      }
+    });
+  }
+  //   console.log(result.term);
+});
